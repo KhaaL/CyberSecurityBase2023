fd60a0138878e2201b2b2863a525d759d8f11ffb
KhaaL <khalid.rashid@gmail.com>
2023-03-13 20:05:03 +0100

    .

--- part2-11.banktransfer/src/pages/views.py
+++ part2-11.banktransfer/src/pages/views.py
@@ -5,18 +5,31 @@ from .models import Account
 
 
 # Create your views here.
-
+# 📄 Relevant documentation: https://docs.djangoproject.com/en/4.1/topics/db/transactions/#tying-transactions-to-http-requests
+@transaction.atomic
 def transfer(sender, receiver, amount):
 	# if amount < 0 then stop.
 	acc1 = Account.objects.get(iban=sender)
 	acc2 = Account.objects.get(iban=receiver)
+	print ("acc1 balance is ", acc1.balance)
+	print ("acc2 balance is ", acc2.balance)
+	print ("amount is ", amount)
+
+	if amount < 0:
+		print ("🟡 negative amount")
+
+	elif amount > acc1.balance:
+		print ("🟡 Transfer larger than the balance")
+
+	elif acc1 == acc2:
+		print ("🟡 Illegal transfer to self")
 
 
-	acc1.balance -= amount
-	acc2.balance += amount
-
-	acc1.save()
-	acc2.save()
+	else:
+		acc1.balance -= amount
+		acc2.balance += amount
+		acc1.save()
+		acc2.save()
 
 
 def homePageView(request):
